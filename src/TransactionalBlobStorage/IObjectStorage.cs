8d2c86b532124947f8fd90cfc14ce481d9c5b73d
Michael <adoser2@hotmail.es>
2023-05-19 22:57:40 +0200

    Remove access modifiers in IObjectStorage

--- src/TransactionalBlobStorage/IObjectStorage.cs
+++ src/TransactionalBlobStorage/IObjectStorage.cs
@@ -2,8 +2,8 @@
 {
     public interface IObjectStorage
     {
-        public Task<Stream?> Get(string fullFileName);
-        public Task<Stream> GetOrThrow(string fullFileName);
+        Task<Stream?> Get(string fullFileName);
+        Task<Stream> GetOrThrow(string fullFileName);
         Task<string> Upload(string fullFileName, Stream stream);
         Task Delete(string fullFileName);
     }
